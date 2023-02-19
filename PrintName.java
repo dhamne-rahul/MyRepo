7a04b29b81c63b4138e2f43599f598cd792420b2
Rahul Dhamne <rdhamne29@gmail.com>
2023-02-19 22:26:46 +0530

    PrintName.java added

--- PrintName.java
+++ PrintName.java
@@ -3,5 +3,6 @@ package Basics;
 public class PrintName {
     public static void main(String[] args) {
         System.out.println("My name is Rahul");
+        System.out.println("is change is noticed");
     }
 }
